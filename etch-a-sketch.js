d3e36891ba0d411ecd1de341497087c9657e514b
CheyenneClarkZone <57722115+CheyenneClarkZone@users.noreply.github.com>
2019-12-20 10:30:12 +0000

    Attached a variable to canvas, and seletced the ID of 'etch-a-sketch

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,11 @@
-// Select the elements on the page - canvas
+// Select the elements on the page - canvas, shake button
+const canvas = document.querySelector('#etch-a-sketch')
+// Setup our canvas for drawing
+
+// write a draw function
+
+// write a handler for the keys
+
+// clear / shake function 
+
+// listen for arrow keys
