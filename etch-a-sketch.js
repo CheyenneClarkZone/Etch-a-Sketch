a4cd93d5df4218dd3b584eae562f0c3fc45d2896
CheyenneClarkZone <57722115+CheyenneClarkZone@users.noreply.github.com>
2019-12-30 11:24:56 +0000

    Add the true constant and begin making switch

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,7 +2,7 @@
 const canvas = document.querySelector('#etch-a-sketch')
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
-
+const MOVE_AMOUNT = 10; /* True constant (value will never change) use uppercase and underscores */  
 // Setup our canvas for drawing
 // make a variable called height and width from the same properties on our canvas.
 const { width, height} = canvas; // Destructuring. Take width property and put it into a variable called width. Same for height.
@@ -14,7 +14,7 @@ const y = Math.floor(Math.random() * height);
 
 ctx.linejoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUNT;
 
 ctx.beginPath(); //start the drawing
 ctx.lineTo(x, y);
@@ -29,9 +29,11 @@ function draw({key}) { //object destructuring - taken properties and renamed the
     ctx.moveTo(x, y);
 
     //move our x and y values depending on what the user did
-    x = x - 10;
-    y = y - 10;
-    ctx.lineTo(x,y);
+    switch (key) {
+        case 'ArrowUp'
+        y = y - MOVE_AMOUNT;
+    }
+    ctx.lineTo(x, y);
     ctx.stroke();
 }
 
